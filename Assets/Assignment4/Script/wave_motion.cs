79848a79833a7f8fae26ae3b99207e64651bd3fb
Steve Marthers <1187575109@qq.com>
2025-05-10 16:22:47 +0800

    wave

--- Assets/Assignment4/Script/wave_motion.cs
+++ Assets/Assignment4/Script/wave_motion.cs
@@ -6,7 +6,7 @@ public class wave_motion : MonoBehaviour
 	int size 		= 100;
 	float rate 		= 0.005f;
 	float gamma		= 0.004f;
-	float damping 	= 0.996f;
+	float damping 	= 0.996f;	// Voscosity 
 	float[,] 	old_h;
 	float[,]	low_h;
 	float[,]	vh;
@@ -16,7 +16,7 @@ public class wave_motion : MonoBehaviour
 	float[,]	cg_p;
 	float[,]	cg_r;
 	float[,]	cg_Ap;
-	bool 	tag=true;
+	bool 	tag = true;
 
 	Vector3 	cube_v = Vector3.zero;
 	Vector3 	cube_w = Vector3.zero;
@@ -31,26 +31,26 @@ public class wave_motion : MonoBehaviour
 		Vector3[] X=new Vector3[size*size];
 
 		for (int i=0; i<size; i++)
-		for (int j=0; j<size; j++) 
-		{
-			X[i*size+j].x=i*0.1f-size*0.05f;
-			X[i*size+j].y=0;
-			X[i*size+j].z=j*0.1f-size*0.05f;
-		}
+			for (int j=0; j<size; j++) 
+			{
+				X[i*size+j].x=i*0.1f-size*0.05f;
+				X[i*size+j].y=0;
+				X[i*size+j].z=j*0.1f-size*0.05f;
+			}
 
 		int[] T = new int[(size - 1) * (size - 1) * 6];
 		int index = 0;
 		for (int i=0; i<size-1; i++)
-		for (int j=0; j<size-1; j++)
-		{
-			T[index*6+0]=(i+0)*size+(j+0);
-			T[index*6+1]=(i+0)*size+(j+1);
-			T[index*6+2]=(i+1)*size+(j+1);
-			T[index*6+3]=(i+0)*size+(j+0);
-			T[index*6+4]=(i+1)*size+(j+1);
-			T[index*6+5]=(i+1)*size+(j+0);
-			index++;
-		}
+			for (int j=0; j<size-1; j++)
+			{
+				T[index*6+0]=(i+0)*size+(j+0);
+				T[index*6+1]=(i+0)*size+(j+1);
+				T[index*6+2]=(i+1)*size+(j+1);
+				T[index*6+3]=(i+0)*size+(j+0);
+				T[index*6+4]=(i+1)*size+(j+1);
+				T[index*6+5]=(i+1)*size+(j+0);
+				index++;
+			}
 		mesh.vertices  = X;
 		mesh.triangles = T;
 		mesh.RecalculateNormals ();
@@ -66,37 +66,37 @@ public class wave_motion : MonoBehaviour
 		cg_Ap 	= new float[size,size];
 
 		for (int i=0; i<size; i++)
-		for (int j=0; j<size; j++) 
-		{
-			low_h[i,j]=99999;
-			old_h[i,j]=0;
-			vh[i,j]=0;
-		}
+			for (int j=0; j<size; j++) 
+			{
+				low_h[i,j]=99999;
+				old_h[i,j]=0;
+				vh[i,j]=0;
+			}
 	}
 
 	void A_Times(bool[,] mask, float[,] x, float[,] Ax, int li, int ui, int lj, int uj)
 	{
 		for(int i=li; i<=ui; i++)
-		for(int j=lj; j<=uj; j++)
-		if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
-		{
-			Ax[i,j]=0;
-			if(i!=0)		Ax[i,j]-=x[i-1,j]-x[i,j];
-			if(i!=size-1)	Ax[i,j]-=x[i+1,j]-x[i,j];
-			if(j!=0)		Ax[i,j]-=x[i,j-1]-x[i,j];
-			if(j!=size-1)	Ax[i,j]-=x[i,j+1]-x[i,j];
-		}
+			for(int j=lj; j<=uj; j++)
+				if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
+				{
+					Ax[i,j]=0;
+					if(i!=0)		Ax[i,j]-=x[i-1,j]-x[i,j];
+					if(i!=size-1)	Ax[i,j]-=x[i+1,j]-x[i,j];
+					if(j!=0)		Ax[i,j]-=x[i,j-1]-x[i,j];
+					if(j!=size-1)	Ax[i,j]-=x[i,j+1]-x[i,j];
+				}
 	}
 
 	float Dot(bool[,] mask, float[,] x, float[,] y, int li, int ui, int lj, int uj)
 	{
 		float ret=0;
 		for(int i=li; i<=ui; i++)
-		for(int j=lj; j<=uj; j++)
-		if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
-		{
-			ret+=x[i,j]*y[i,j];
-		}
+			for(int j=lj; j<=uj; j++)
+				if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
+				{
+					ret+=x[i,j]*y[i,j];
+				}
 		return ret;
 	}
 
@@ -106,11 +106,11 @@ public class wave_motion : MonoBehaviour
 		A_Times(mask, x, cg_r, li, ui, lj, uj);
 
 		for(int i=li; i<=ui; i++)
-		for(int j=lj; j<=uj; j++)
-		if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
-		{
-			cg_p[i,j]=cg_r[i,j]=b[i,j]-cg_r[i,j];
-		}
+			for(int j=lj; j<=uj; j++)
+				if(i>=0 && j>=0 && i<size && j<size && mask[i,j])
+				{
+					cg_p[i,j]=cg_r[i,j]=b[i,j]-cg_r[i,j];
+				}
 
 		float rk_norm=Dot(mask, cg_r, cg_r, li, ui, lj, uj);
 
@@ -145,24 +145,46 @@ public class wave_motion : MonoBehaviour
 	void Shallow_Wave(float[,] old_h, float[,] h, float [,] new_h)
 	{		
 		//Step 1:
-		//TODO: Compute new_h based on the shallow wave model.
-
+		// TODO: Compute new_h based on the shallow wave model.
+		// Why size*size instead of 3*3 ???
+		float sumH = 0.0f;
+		for(int i = 0; i < size; i++)
+			for(int j = 0; j < size; j++)
+			{	
+				if(i!=0 && j!=0 && i!=size-1 && j!=size-1)
+				{
+					sumH = 0.0f;
+					sumH = h[i+1,j] + h[i-1,j] + h[i,j+1] + h[i,j-1] - 4*h[i,j];
+					new_h[i,j] = h[i,j] + (h[i,j] - old_h[i,j]) * damping + sumH * rate;
+				}
+			}
+		
 		//Step 2: Block->Water coupling
 		//TODO: for block 1, calculate low_h.
 		//TODO: then set up b and cg_mask for conjugate gradient.
 		//TODO: Solve the Poisson equation to obtain vh (virtual height).
+		
 
 		//TODO: for block 2, calculate low_h.
 		//TODO: then set up b and cg_mask for conjugate gradient.
 		//TODO: Solve the Poisson equation to obtain vh (virtual height).
-	
+
+
 		//TODO: Diminish vh.
+
 
 		//TODO: Update new_h by vh.
 
+
+
 		//Step 3
 		//TODO: old_h <- h; h <- new_h;
-
+		for(int i = 0; i < size; ++i)
+			for(int j = 0; j < size; ++j)
+			{
+				h[i,j] = new_h[i,j];
+				old_h[i,j] = h[i,j];
+			}
 		//Step 4: Water->Block coupling.
 		//More TODO here.
 	}
@@ -176,11 +198,26 @@ public class wave_motion : MonoBehaviour
 		float[,] new_h = new float[size, size];
 		float[,] h     = new float[size, size];
 
-		//TODO: Load X.y into h.
+		// TODO: Load X.y into h.
+		for(int i = 0; i < size; ++i)
+			for(int j = 0; j < size; ++j)
+			{
+				h[i,j] = X[i*size + j].y;
+			}
 
 		if (Input.GetKeyDown ("r")) 
 		{
-			//TODO: Add random water.
+			// TODO: Add random water.
+			int i = Random.Range(1, size-1);
+			int j = Random.Range(1, size-1);
+			float r = Random.Range(0.3f, 0.5f);
+			h[i,j] += 9 * r;
+
+			for(int a = 0; a < 3; a++)
+				for(int b = 0; b < 3; b++)
+				{
+					h[i-1+a, j-1+b] -= r;
+				}
 		}
 	
 		for(int l=0; l<8; l++)
@@ -189,7 +226,13 @@ public class wave_motion : MonoBehaviour
 		}
 
 		//TODO: Store h back into X.y and recalculate normal.
+		for(int i = 0; i < size; ++i)
+			for(int j = 0; j < size; ++j)
+			{
+				 X[i*size + j].y = h[i,j];
+			}
 
-		
+		mesh.vertices = X;
+		mesh.RecalculateNormals();	// Unity Build_In API
 	}
 }
